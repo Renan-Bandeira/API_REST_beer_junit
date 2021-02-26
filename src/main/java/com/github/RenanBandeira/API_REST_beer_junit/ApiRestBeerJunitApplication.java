440a11db5e69be94de6ffa9398729d37bde2249b
renanbandeira94 <67717214+Renan-Bandeira@users.noreply.github.com>
2021-02-26 15:40:06 -0300

    First commit

---
+++ src/main/java/com/github/RenanBandeira/API_REST_beer_junit/ApiRestBeerJunitApplication.java
@@ -0,0 +1,13 @@
+package com.github.RenanBandeira.API_REST_beer_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBeerJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBeerJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/RenanBandeira/API_REST_beer_junit/ApiRestBeerJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RenanBandeira.API_REST_beer_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBeerJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
